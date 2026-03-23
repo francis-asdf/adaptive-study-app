831dfc798705096648df5ba45bd22bb66a2ceaf4
francis-asdf <114354354+francis-asdf@users.noreply.github.com>
2026-03-22 22:07:57 -0700

    connect server to PostgreSQL database

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,43 @@
 import express from 'express';
+import pool from './db.js';
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-let tasks = [];
-
 // create task
-app.post('/tasks', (req, res) => {
-    const task = req.body;
-    tasks.push(task);
-    res.json(task);
+app.post('/tasks', async (req, res) => {
+    const { text } = req.body;
+    const result = await pool.query(
+        'INSERT INTO tasks (text) values ($1) RETURNING *',
+        [text]
+    );
+    res.json(result.rows[0]);
 });
 
 // get tasks
-app.get('/tasks', (req, res) => {
-    res.json(tasks);
+app.get('/tasks', async (req, res) => {
+    const result = await pool.query('SELECT * FROM tasks');
+    res.json(result.rows);
 });
 
 // update task
-app.put('/tasks', (req, res) => {
-    const id = req.params.id;
-    tasks[id] = req.body;
-    res.json(tasks[id]);
+app.put('/tasks/:id', async (req, res) => {
+    const { id } = req.params;
+    const { text } = req.body;
+    const result = await pool.query(
+        'UPDATE tasks SET text = $1 WHERE id = $2 RETURNING *',
+        [text, id]
+    );
+    res.json(result.rows[0]);
+});
+
+// delete task
+app.delete('/tasks/:id', async (req, res) => {
+    const { id } = req.params;
+    await pool.query('DELETE FROM tasks WHERE id = $1', [id]);
+    res.json({ message: 'Task deleted' });
 });
 
 app.listen(PORT, () => {
