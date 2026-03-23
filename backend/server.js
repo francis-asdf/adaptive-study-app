1ff26ea3bf0b4a22da7016d2aaa215f464fcf479
francis-asdf <114354354+francis-asdf@users.noreply.github.com>
2026-03-23 11:54:40 -0700

    add task bullet

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,26 @@
 import express from 'express';
 import pool from './db.js';
+import cors from 'cors';
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 // create task
 app.post('/tasks', async (req, res) => {
-    const { text } = req.body;
-    const result = await pool.query(
-        'INSERT INTO tasks (text) values ($1) RETURNING *',
-        [text]
-    );
-    res.json(result.rows[0]);
+    try {
+        const { text } = req.body;
+        const result = await pool.query(
+            'INSERT INTO tasks (text) values ($1) RETURNING *',
+            [text]
+        );
+        res.json(result.rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Server error' });
+    }
 });
 
 // get tasks
@@ -41,5 +48,5 @@ app.delete('/tasks/:id', async (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on https://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
--- frontend/script.js
+++ frontend/script.js
@@ -2,11 +2,16 @@ async function addTask() {
     const input = document.getElementById("task-input");
     const task = { text: input.value };
 
-    await fetch('http://localhost:3000/tasks', {
+    const res = await fetch('http://localhost:3000/tasks', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(task)
     });
+    const newTask = await res.json();
+
+    const li = document.createElement("li");
+    li.textContent = newTask.text;
+    document.getElementById("task-list").appendChild(li);
 
     input.value = "";
 }
